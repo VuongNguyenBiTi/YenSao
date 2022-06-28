4d5a164ebd1239add09f45f514482285e84f67e0
lthchinchin <letrunghieu5612@gmail.com>
2022-06-28 22:01:15 +0700

    test sync file

---
+++ footer.php
@@ -0,0 +1,21 @@
+        <!-- Begin Footer -->
+        <footer class="footer">
+        </footer>
+        <!-- End Footer -->
+
+        <div class="contact-fix">
+        </div>
+        <!-- bottom to top -->
+        <a href="#">
+            <div id="bottom_to_top" style="bottom: 30px;">
+                <i class="fas fa-arrow-up text-white"></i>
+            </div>
+        </a>
+        <script>
+            var ajaxUrl = '<?php echo admin_url('admin-ajax.php') ?>'
+        </script>
+        <?php wp_footer(); ?>
+        </div>
+        </body>
+
+        </html>
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <?php get_header(); ?>
 <main>
-
+    day la trang chu
 </main>
 
 <?php get_footer(); ?>
