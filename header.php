5f2f125371c507e604f3b63208566b04fd5947f9
lthchinchin <letrunghieu5612@gmail.com>
2022-06-28 21:11:52 +0700

    first commit

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <title><?php bloginfo('name'); ?> - <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Raleway:ital,wght@0,400;0,600;0,800;1,400;1,600;1,800&display=swap" rel="stylesheet">
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class('mb-0'); ?>>
+    <div id="wrapper">
+        <!-- Begin Header -->
+        <header class="header">
+        </header>
+        <!-- End Header -->
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php /* Template Name: Trang chủ */ ?>
+
+<?php get_header(); ?>
+<main>
+
+</main>
+
+<?php get_footer(); ?>
