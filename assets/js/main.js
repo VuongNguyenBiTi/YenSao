db85819174ab6dc452810d70261a5a8670db41e0
Vương Nguyễn <vuongnguyenbiti@gmail.com>
2023-08-30 09:53:35 +0700

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,64 +1,6 @@
 (function ($) {
-	$(document).ready(function () {
-		/* owlcarousel */
-		if ($(".banner-carousel").length) {
-			$(".banner-carousel").each(function () {
-				// var owl = $(".home-carousel");
-				$(this).owlCarousel({
-					loop:true,
-					margin:10,
-					nav: true,
-					items: 1,
-					
-				});
-			});
-		}
-
-		//home firm
-		if ($(".firm-carousel").length) {
-			$(".firm-carousel").each(function () {
-				// var owl = $(".home-carousel");
-				$(this).owlCarousel({
-					loop:true,
-					margin:10,
-					nav: true,
-					items: 5,
-					
-				});
-			});
-		}
-
-		/* AOS Animate */
-		AOS.init({ once: true });
-
-		/* Show menu mobile */
-		$("#mobile-menu-button").click(function () {
-			$(this).toggleClass("active");
-			$(".header-menu").toggleClass("active");
-		});
-	});
-
-	// scroll effect header , bottomToTop , Btn-call
-	window.onscroll = function () {
-		myFunction();
-	};
-	var toTop = document.getElementById("bottom_to_top");
-	function myFunction() {
-		// console.log(this.scrollY);
-		if (this.scrollY > 100) {
-			toTop.style.bottom = "30px";
-		} else {
-			toTop.style.bottom = "-100px";
-		}
-		this.oldScroll = this.scrollY;
-	}
-
-	// toggle search bar
-	$('#search-btn').click(() => {
-		$('.header-search').fadeToggle();
-	})
-	$('#close-search-bar').click(() => {
-		$('.header-search').fadeOut();
-	})
-
+  $(document).ready(function () {
+    /* AOS Animate */
+    AOS.init({ once: true });
+  });
 })(jQuery);
--- footer.php
+++ footer.php
@@ -1,103 +1,11 @@
-        <!-- fixed feedback -->
-        <button class="btn-feedback btn btn-cus btn-cus--primary2" aria-label="Give feedback on the Financial Times website" data-trackable="feedback-start">
-            <span>Feedback</span>
-        </button>
-        <!-- Begin Footer -->
-        <footer class="footer">
-            <div class="footer-top">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-2">
-                            <h5 class="footer-top-title">
-                                Support
-                            </h5>
-                            <ul class="footer-top-menu-items">
-                                <li><a href="#">View site tip</a></li>
-                                <li><a href="#">View site tip</a></li>
-                                <li><a href="#">View site tip</a></li>
-                                <li><a href="#">View site tip</a></li>
-                            </ul>
-                        </div>
-                        <div class="col-lg-2">
-                            <h5 class="footer-top-title">
-                                Legal & Privacy
-                            </h5>
-                            <ul class="footer-top-menu-items">
-                                <li><a href="#">View site tip</a></li>
-                                <li><a href="#">View site tip</a></li>
-                                <li><a href="#">View site tip</a></li>
-                            </ul>
-                        </div>
-                        <div class="col-lg-2">
-                            <h5 class="footer-top-title">
-                                Services
-                            </h5>
-                            <ul class="footer-top-menu-items">
-                                <li><a href="#">View site tip</a></li>
-                                <li><a href="#">View site tip</a></li>
-                                <li><a href="#">View site tip</a></li>
-                                <li><a href="#">View site tip</a></li>
-                                <li><a href="#">View site tip</a></li>
-                                <li><a href="#">View site tip</a></li>
-                                <li><a href="#">View site tip</a></li>
-                            </ul>
-                        </div>
-                        <div class="col-lg-2">
-                            <h5 class="footer-top-title">
-                                Tools
-                            </h5>
-                            <ul class="footer-top-menu-items">
-                                <li><a href="#">View site tip</a></li>
-                                <li><a href="#">View site tip</a></li>
-                                <li><a href="#">View site tip</a></li>
-                                <li><a href="#">View site tip</a></li>
-                            </ul>
-                        </div>
-                        <div class="col-lg-2">
-                            <h5 class="footer-top-title">
-                                Community & Events
-                            </h5>
-                            <ul class="footer-top-menu-items">
-                                <li><a href="#">View site tip</a></li>
-                                <li><a href="#">View site tip</a></li>
-                                <li><a href="#">View site tip</a></li>
-                                <li><a href="#">View site tip</a></li>
-                                <li><a href="#">View site tip</a></li>
-                                <li><a href="#">View site tip</a></li>
-                            </ul>
-                        </div>
-                        <div class="col-lg-2">
-                            <h5 class="footer-top-title">
-                                More
-                            </h5>
-                            <ul class="footer-top-menu-items">
-                                <li><a href="#">View site tip</a></li>
-                                <li><a href="#">View site tip</a></li>
-                                <li><a href="#">View site tip</a></li>
-                            </ul>
-                        </div>
+<!-- Begin Footer -->
+<footer class="footer">
+    <h1>Footer</h1>
+</footer>
+<!-- End Footer -->
 
-                    </div>
-                </div>
-            </div>
-            <div class="footer-bottom">
-            </div>
-        </footer>
-        <!-- End Footer -->
+<?php wp_footer(); ?>
+</div>
+</body>
 
-        <div class="contact-fix">
-        </div>
-        <!-- bottom to top -->
-        <a href="#">
-            <div id="bottom_to_top" style="bottom: 30px;">
-                <i class="fas fa-arrow-up text-white"></i>
-            </div>
-        </a>
-        <script>
-            var ajaxUrl = '<?php echo admin_url('admin-ajax.php') ?>'
-        </script>
-        <?php wp_footer(); ?>
-        </div>
-        </body>
-
-        </html>
+</html>
